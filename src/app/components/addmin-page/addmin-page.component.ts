3f26efe2f418d9875cdce2ea92070d3b26b699e2
Nguyễn Văn Kim Hải <shinichi24567@gmail.com>
2020-01-29 17:37:34 +0700

    cap nhat chuc nang logout, hien ten

--- src/app/components/addmin-page/addmin-page.component.ts
+++ src/app/components/addmin-page/addmin-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthServiceService } from './../auth/auth-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-addmin-page',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addmin-page.component.css']
 })
 export class AddminPageComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private authService: AuthServiceService, private router: Router) { }
+  fullName: string ='';
   ngOnInit() {
+    this.fullName = JSON.parse(localStorage.getItem('userData1')).fullName;
   }
-
+  onLogOut() {
+    this.authService.user.next(null);
+    localStorage.removeItem('token')
+    localStorage.removeItem('userData1')
+    this.router.navigate(['/'])
+  }
 }
--- src/app/components/user-page/user-page.component.ts
+++ src/app/components/user-page/user-page.component.ts
@@ -3,6 +3,7 @@ import { AuthServiceService } from './../auth/auth-service.service';
 import { UserService } from './user.service';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { orderCreateData } from './orderCreateData.model';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
@@ -11,13 +12,21 @@ import { orderCreateData } from './orderCreateData.model';
 export class UserPageComponent implements OnInit {
   role = '';
   user: any;
+  fullName: string ='';
  
- 
-  constructor(private userService: UserService, private authService: AuthServiceService) { }
+  constructor(private userService: UserService, private authService: AuthServiceService,private router:Router) { }
   ngOnInit() {
     this.authService.user.subscribe(resp => {
       this.role = resp.role
+    
     })
+    this.fullName = JSON.parse(localStorage.getItem('userData1')).fullName;
+    console.log(this.fullName)
   }
- 
+  onLogOut() {
+    this.authService.user.next(null);
+    localStorage.removeItem('token')
+    localStorage.removeItem('userData1')
+    this.router.navigate(['/'])
+  }
 }
